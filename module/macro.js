0b3a441c4d0de22a1ae93ce0a5fdd1209966f32d
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2021-09-24 13:09:53 -0600

    split out offhandAttackDialog func

--- module/macro.js
+++ module/macro.js
@@ -4,49 +4,27 @@
  * @param {Object} data     The dropped data
  * @param {number} slot     The hotbar slot to use
  * @returns {Promise}
+ * 
+ * Weapon attack macro commands call weaponAttackMacro() with
+ * 'weaponId' and an options arg with:
+ * {
+ *  offhand: true/false,
+ *  dismount: true/false,
+ *  throwable: true/false,
+ *  attack_type: melee/missile/thrown,
+ *  attack_description: slash/thrust/bash/sling/bow/crossbow
+ * }
  */
 export async function createLostlandsMacro(data, slot) {
   if(data.type === 'Macro') return false;
   const item = data.data;
   const itemName = item?.name || '';
-  const itemMacro = item?.data.macro;
-  let itemMacroWithId;
+  const itemMacro = item?.data.macro || '';
+  let itemMacroWithId = '';
   if(item?._id && itemMacro) itemMacroWithId = itemMacro.replace('itemId', item._id);
 
-  // check for offhand weapon macro
-  const offhandWeaps = ['dagger','shortsword','javelin','handaxe','hammer','mace'];
-  let offhandWeap = false;
-  for (const weap of offhandWeaps) {
-    if (itemName.toLowerCase().includes(weap)) offhandWeap = true;
-  }
-  if(offhandWeap && !data.skipOffhandDialog) {
-    return new Dialog({
-      title: "What form of attack?",
-      content: 
-        ``,
-      buttons: {
-        one: {
-          icon: '<i class="fas fa-hand-point-right"></i>',
-          label: "Main-hand",
-          callback: () => {
-            data.skipOffhandDialog = true;
-            createLostlandsMacro(data, slot)
-          }
-        },
-        two: {
-          icon: '<i class="fas fa-hand-point-left"></i>',
-          label: "Off-hand",
-          callback: () => {
-            data.skipOffhandDialog = true;
-            item.data.macro = "game.lostlands.Macro.offhandWeaponMacro('itemId')";
-            item.name += " (off-hand)";
-            createLostlandsMacro(data, slot)
-          }
-        }
-      },
-      default: "one"
-    }).render(true);
-  }
+  const offhandWeap = itemMacroWithId.replace(/\s+/g, '').includes('offhand:true');
+  if(offhandWeap && !data.skipOffhandDialog) return offhandAttackDialog(data, slot);
 
   // check for dismount macro
   const dismountWeaps = ['halberd','polearm'];
@@ -57,8 +35,7 @@ export async function createLostlandsMacro(data, slot) {
   if(dismountWeap && !data.skipDismountDialog) {
     return new Dialog({
       title: "What form of attack?",
-      content: 
-        ``,
+      content: ``,
       buttons: {
         one: {
           icon: '<i class="fas fa-user-friends"></i>',
@@ -83,13 +60,17 @@ export async function createLostlandsMacro(data, slot) {
     }).render(true);
   }
 
-  let macro = game.macros.find(m => (m.name === itemName && m.data.command === itemMacroWithId));
+  const macroName = itemName || (data.label ? data.label : undefined);
+  const macroCommand = itemMacroWithId || (data.roll ? `/r ${data.roll}#${data.label}` : undefined);
+  const type = data.roll ? "chat" : "script";
+
+  let macro = game.macros.find(m => (m.name === macroName && m.data.command === macroCommand));
   
   if (!macro) {
-    if(itemMacroWithId) macro = await Macro.create({
-      name: data.data.name,
-      type: "script",
-      command: itemMacroWithId,
+    if(macroCommand) macro = await Macro.create({
+      name: macroName,
+      type: type,
+      command: macroCommand,
       flags: { "lostlands.attrMacro": true }
     });
     else {
@@ -125,8 +106,7 @@ export function grappleMacro() {
 export function thrownWeaponMacro(weaponId) {
   new Dialog({
     title: "What form of attack?",
-    content: 
-      ``,
+    content: ``,
     buttons: {
       one: {
         icon: '<i class="fas fa-user-friends"></i>',
@@ -153,7 +133,7 @@ function weaponAttack(weaponId, options={}) {
           <label>Attack modifiers?</label>
           <input type="text" id="atkMod" placeholder="e.g. -4">
         </div>
-        ${options.baseAttack ? '' : `<div class="form-group">
+        ${options.grapple ? '' : `<div class="form-group">
           <label>Damage modifiers?</label>
           <input type="text" id="dmgMod" placeholder="e.g. 2d6">
         </div>
@@ -328,4 +308,187 @@ function weaponAttack(weaponId, options={}) {
       return `background: ${bgColour}; padding: 1px 4px; border: 1px solid #4b4a44; border-radius: 2px; white-space: nowrap; word-break: break-all;`;
     }
   }
+}
+
+function offhandAttackDialog(data, slot) {
+  const item = data.data;
+  return new Dialog({
+    title: "What form of attack?",
+    content: ``,
+    buttons: {
+      one: {
+        icon: '<i class="fas fa-hand-point-right"></i>',
+        label: "Main-hand",
+        callback: () => {
+          data.skipOffhandDialog = true;
+          createLostlandsMacro(data, slot)
+        }
+      },
+      two: {
+        icon: '<i class="fas fa-hand-point-left"></i>',
+        label: "Off-hand",
+        callback: () => {
+          data.skipOffhandDialog = true;
+          item.data.macro = "game.lostlands.Macro.offhandWeaponMacro('itemId')";
+          item.name += " (off-hand)";
+          createLostlandsMacro(data, slot)
+        }
+      }
+    },
+    default: "one"
+  }).render(true);
+}
+
+export function buyBasicEquipment() {
+  let token = canvas.tokens.controlled[0];
+  let actor = canvas.tokens.controlled[0]?.actor;
+  if(!actor || !actor.isOwner){
+    ui.notifications.error("Select the token of the character making the purchase.");
+    return;
+  }
+
+  const basicItems = game.items.filter(i => i.folder?.parentFolder?.name == 'Basic Equipment');
+  const armsAndArmor = basicItems.filter(i => i.folder?.name == 'Arms & Armor');
+  const misc = basicItems.filter(i => i.folder?.name === 'Miscellaneous');
+  
+  let armsAndArmorOptionsText = ``;
+  for (const item of armsAndArmor) {
+    armsAndArmorOptionsText += `<option value="${item.id}">${item.name}</option>`;
+  }
+
+  let miscOptionsText = ``;
+  for (const item of misc) {
+    miscOptionsText += `<option value="${item.id}">${item.name}</option>`;
+  }
+  const optionsText = `
+    <optgroup label="Miscellaneous">${miscOptionsText}</optgroup>
+    <optgroup label="Arms & Armor">${armsAndArmorOptionsText}</optgroup>
+  `;
+
+  async function resolvePurchase(itemId, itemName, qty, price) {
+    qty = +qty;
+    let cost = +price;
+    if(isNaN(qty) || qty < 0 || qty % 1 || isNaN(cost) || cost < 0 || cost % 1){
+      ui.notifications.error("Invalid input.");
+      return;
+    }
+
+    const actorItems = actor.data.items, gpItem = actorItems.find(i => i.name === "Gold Pieces");
+    const gpQty = +gpItem?.data.data.quantity || 0;
+    const spItem = actorItems.find(i => i.name === "Silver Pieces");
+    const spQty = +spItem?.data.data.quantity || 0;
+    const cpItem = actorItems.find(i => i.name === "Copper Pieces");
+    const cpQty = +cpItem?.data.data.quantity || 0;
+    const totalMoney = Math.round((gpQty + spQty/10 + cpQty/50) * 100) / 100;
+
+    if(cost > totalMoney) {
+      ChatMessage.create({
+        speaker: ChatMessage.getSpeaker(token),
+        content: `tries to purchase ${qty} ${itemName}${qty > 1 ? 's' : ''} for ${price} GP, but does not have enough money. The merchant appears annoyed.`,
+        type: CONST.CHAT_MESSAGE_TYPES.EMOTE
+      });
+      return;
+    }
+
+    // add item to actor
+    const ownedItem = actorItems.find(i => i.name === itemName);
+    if(ownedItem) {
+      const ownedItemQty = +ownedItem.data.data.quantity;
+      const itemUpdate = { _id: ownedItem.data._id, "data.quantity": ownedItemQty + qty };
+      await actor.updateEmbeddedDocuments("Item", [itemUpdate]);
+    } else {
+      const itemData = game.items.get(itemId).clone({"data.quantity": qty});
+      await actor.createEmbeddedDocuments("Item", [itemData.data]);
+    }
+
+    // pay for item from actor
+    if(cpQty >= cost*50) {
+      const cpUpdate = { _id: cpItem?.data._id, "data.quantity": cpQty - cost*50 };
+      cpUpdate._id && await actor.updateEmbeddedDocuments("Item", [cpUpdate]);
+    } else if(Math.round((spQty/10 + cpQty/50) * 100) / 100 >= cost) {
+      const cpUpdate = { _id: cpItem?.data._id, "data.quantity": cpQty % 5 };
+      cpUpdate._id && await actor.updateEmbeddedDocuments("Item", [cpUpdate]);
+      cost = Math.round((cost - Math.floor(cpQty / 5) * 5 / 50) * 100) / 100;
+      const spUpdate = { _id: spItem?.data._id, "data.quantity": spQty - cost*10 };
+      spUpdate._id && await actor.updateEmbeddedDocuments("Item", [spUpdate]);
+    } else {
+      let change = Math.round((totalMoney - cost) * 100) / 100;
+      let gpChange = Math.floor(change);
+      change = Math.round((change - gpChange) * 10 * 100) / 100;
+      let spChange = Math.floor(change);
+      change = Math.round((change - spChange) * 5 * 100) / 100;
+      const gpUpdate = { _id: gpItem?.data._id, "data.quantity": gpChange };
+      gpUpdate._id && await actor.updateEmbeddedDocuments("Item", [gpUpdate]);
+      const spUpdate = { _id: spItem?.data._id, "data.quantity": spChange };
+      spUpdate._id && await actor.updateEmbeddedDocuments("Item", [spUpdate]);
+      const cpUpdate = { _id: cpItem?.data._id, "data.quantity": change };
+      cpUpdate._id && await actor.updateEmbeddedDocuments("Item", [cpUpdate]);
+    }
+
+    const content = `buys ${qty} ${itemName}${qty > 1 ? 's' : ''} for ${price} GP.`;
+    ChatMessage.create({
+      speaker: ChatMessage.getSpeaker(token),
+      content: content,
+      type: CONST.CHAT_MESSAGE_TYPES.EMOTE
+    });
+  }
+
+  new Dialog({
+    title: "Buy Basic Equipment",
+    content: 
+      `<p><form id="purchase-form">
+        <div class="flexrow">
+          <div class="flexcol flex1">
+            <label for="qty">Quantity</label>
+            <input id="qty" type="number" value="1"/>
+          </div>
+          <div class="flexcol flex2" style="padding-right:2px">
+            <label for="select">Item</label>
+            <select id="select" style="padding:1px">
+              ${optionsText}
+            </select>
+          </div>
+          <div class="flexcol flex1">
+            <label for="cost">Cost</label>
+            <input id="cost" type="number"/>
+          </div>
+        </div>
+      </form></p>`,
+    buttons: {
+      one: {
+        icon: '<i class="fas fa-check"></i>',
+        label: "Buy",
+        callback: async html => {
+          const itemId = html.find("#select").val();
+          const itemName = html.find("#select option:selected").text();
+          const qty = html.find("#qty").val();
+          const unitCost = html.find("#cost").val();
+          const cost = qty && unitCost ? qty * unitCost : undefined;
+          await resolvePurchase(itemId, itemName, qty, cost);
+        }
+      },
+      two: {
+        icon: '<i class="fas fa-times"></i>',
+        label: "Cancel"
+      }
+    },
+    default: "two",
+    render: html => {
+      const select = html.find("#select");
+      const costInput = html.find("#cost");
+      syncCostVal();
+
+      select.change(function() {
+        syncCostVal();
+      })
+
+      function syncCostVal() {
+        const itemId = select.val();
+        const selectedItem = game.items.get(itemId);
+        const itemCost = selectedItem.data.data.attributes.value?.value;
+        if(!itemCost) ui.notifications.error("Item does not have the value attribute set.");
+        costInput.val(itemCost);
+      }
+    }
+  }).render(true);
 }
