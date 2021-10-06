ab5e3254611bb5fc77df88109187ae84b786e225
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2021-10-05 23:57:25 -0600

    change sv to st

--- module/constants.js
+++ module/constants.js
@@ -1,13 +1,13 @@
 export const ATTRIBUTE_TYPES = ["String", "Number", "Boolean", "Formula", "Resource"];
 export const MAX_SPELL_LEVELS = {'spell_witch': 6, 'spell_cleric': 5, 'spell_magic': 9};
 export const FIGHTER_XP_PROGRESSION = [
-  {xpRequired: 240000, updateData: {"data.level": 9, "data.bab": 9, "data.sv": 9, "data.xp.max": 360000}},
-  {xpRequired: 120000, updateData: {"data.level": 8, "data.bab": 8, "data.sv": 9, "data.xp.max": 240000}},
-  {xpRequired: 60000, updateData: {"data.level": 7, "data.bab": 7, "data.sv": 10, "data.xp.max": 120000}},
-  {xpRequired: 30000, updateData: {"data.level": 6, "data.bab": 6, "data.sv": 11, "data.xp.max": 60000}},
-  {xpRequired: 15000, updateData: {"data.level": 5, "data.bab": 5, "data.sv": 11, "data.xp.max": 30000}},
-  {xpRequired: 7000, updateData: {"data.level": 4, "data.bab": 4, "data.sv": 12, "data.xp.max": 15000}},
-  {xpRequired: 3000, updateData: {"data.level": 3, "data.bab": 3, "data.sv": 13, "data.xp.max": 7000}},
-  {xpRequired: 1000, updateData: {"data.level": 2, "data.bab": 2, "data.sv": 13, "data.xp.max": 3000}},
-  {xpRequired: 0, updateData: {"data.level": 1, "data.bab": 1, "data.sv": 14, "data.xp.max": 1000}}
+  {xpRequired: 240000, updateData: {"data.level": 9, "data.bab": 9, "data.st": 9, "data.xp.max": 360000}},
+  {xpRequired: 120000, updateData: {"data.level": 8, "data.bab": 8, "data.st": 9, "data.xp.max": 240000}},
+  {xpRequired: 60000, updateData: {"data.level": 7, "data.bab": 7, "data.st": 10, "data.xp.max": 120000}},
+  {xpRequired: 30000, updateData: {"data.level": 6, "data.bab": 6, "data.st": 11, "data.xp.max": 60000}},
+  {xpRequired: 15000, updateData: {"data.level": 5, "data.bab": 5, "data.st": 11, "data.xp.max": 30000}},
+  {xpRequired: 7000, updateData: {"data.level": 4, "data.bab": 4, "data.st": 12, "data.xp.max": 15000}},
+  {xpRequired: 3000, updateData: {"data.level": 3, "data.bab": 3, "data.st": 13, "data.xp.max": 7000}},
+  {xpRequired: 1000, updateData: {"data.level": 2, "data.bab": 2, "data.st": 13, "data.xp.max": 3000}},
+  {xpRequired: 0, updateData: {"data.level": 1, "data.bab": 1, "data.st": 14, "data.xp.max": 1000}}
 ];
