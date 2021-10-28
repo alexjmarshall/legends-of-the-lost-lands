d9700904b6d8116348039639b73df2c3acf9b97f
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2021-10-28 16:47:01 -0600

    fix container actor sheet

--- module/container-actor-sheet.js
+++ module/container-actor-sheet.js
@@ -1,6 +1,7 @@
-import { SimpleActorSheet } from "./actor-sheet.js";
+import { EntitySheetHelper } from "./helper.js";
+import * as Constant from "./constants.js";
 
-export class ContainerActorSheet extends SimpleActorSheet {
+export class ContainerActorSheet extends ActorSheet {
   /** @inheritdoc */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
@@ -12,5 +13,126 @@ export class ContainerActorSheet extends SimpleActorSheet {
       scrollY: [".description", ".items", ".attributes"],
       dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
     });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  getData() {
+    const context = super.getData();
+    EntitySheetHelper.getAttributeData(context.data);
+    context.shorthand = !!game.settings.get("lostlands", "macroShorthand");
+    context.systemData = context.data.data;
+    context.dtypes = Constant.ATTRIBUTE_TYPES;
+    context.isGM = game.user.isGM;
+    context.isPlayer = !context.isGM;
+    context.data.items = context.data.items.filter(i => i.type === 'item');
+
+    return context;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if ( !this.isEditable ) return;
+
+    // Attribute Management
+    html.find(".attributes").on("click", ".attribute-control", EntitySheetHelper.onClickAttributeControl.bind(this));
+    html.find(".groups").on("click", ".group-control", EntitySheetHelper.onClickAttributeGroupControl.bind(this));
+    html.find(".attributes").on("click", "a.attribute-roll", EntitySheetHelper.onAttributeRoll.bind(this));
+
+    // Item Controls
+    html.find(".item-control").click(this._onItemControl.bind(this));
+    html.find(".item-row").dblclick(this._onItemControl.bind(this));
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle click events for Item control buttons within the Actor Sheet
+   * @param event
+   * @private
+   */
+   async _onItemControl(event) {
+    event.preventDefault();
+
+    // Obtain event data
+    const button = event.currentTarget;
+    const li = button.closest(".item");
+    const itemId = li?.dataset.itemId;
+    const item = this.actor.items.get(itemId);
+    const type = button.dataset.type;
+    const sheetFlag = {
+      core: {
+        sheetClass: "lostlands.SpellItemSheet"
+      }
+    };
+    const data = {flags:sheetFlag, name: game.i18n.localize("SIMPLE.ItemNew"), type: type};
+    if(type === "item") data.flags = {};
+    
+    // Handle different actions
+    switch ( button.dataset.action ) {
+      case "create":
+        const cls = getDocumentClass("Item");
+        return cls.create(data, {parent: this.actor});
+      case "edit":
+        return item.sheet.render(true);
+      case "delete":
+        const actor = this.actor;
+        const itemQty = +item.data.data.quantity || 0;
+        if(itemQty <= 1) return item.delete();
+        return new Dialog({
+          title: "Delete Item",
+          content: 
+          `<form>
+            <div class="flexrow">
+              <label class="flex1">How many?</label>
+              <label class="flex1" style="text-align:center;" id="splitValue"></label>
+              <input class="flex3" type="range" min="1" id="splitRange">
+            </div>
+          </form>`,
+          buttons: {
+            one: {
+              icon: '<i class="fas fa-check"></i>',
+              label: "Submit",
+              callback: async html => {
+                const quantityVal = +html.find("#splitRange").val();
+                if(quantityVal >= itemQty) return item.delete();
+                await actor.updateEmbeddedDocuments("Item", [{_id: item._id, "data.quantity": itemQty - quantityVal}]);
+              }
+            },
+            two: {
+              icon: '<i class="fas fa-times"></i>',
+              label: "Cancel"
+            }
+          },
+          default: "two",
+          render: html => {
+            const initialVal = itemQty;
+            const splitRange = html.find('#splitRange');
+            splitRange.attr("max",itemQty);
+            splitRange.val(initialVal);
+            const splitValue = html.find('#splitValue');
+            splitValue.html(initialVal);
+            splitRange.on('input', () => {
+              splitValue.html(splitRange.val());
+            });
+          }
+        }).render(true);
+    }
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  _getSubmitData(updateData) {
+    let formData = super._getSubmitData(updateData);
+    formData = EntitySheetHelper.updateAttributes(formData, this.object);
+    formData = EntitySheetHelper.updateGroups(formData, this.object);
+    return formData;
   }
 }
