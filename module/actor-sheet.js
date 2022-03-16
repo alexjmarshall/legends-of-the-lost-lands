0794e42b99ec9be484cc1ce67e60e809a0178c4b
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2022-03-16 09:55:02 -0600

    tweak thirst status language

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -98,7 +98,7 @@ export class SimpleActorSheet extends ActorSheet {
     fatigue.exhaustionDesc = exhaustionStatus === 2 ? 'Exhausted' : exhaustionStatus === 1 ? 'Sleepy' : 'Fine';
 
     const thirstStatus = this.getFatigueStatus(data, 'thirst');
-    fatigue.thirstDesc = thirstStatus === 2 ? 'Dying of thirst' : thirstStatus === 1 ? 'Thirsty' : 'Fine';
+    fatigue.thirstDesc = thirstStatus === 2 ? 'Dehydrated' : thirstStatus === 1 ? 'Thirsty' : 'Fine';
 
     const hungerStatus = this.getFatigueStatus(data, 'hunger');
     fatigue.hungerDesc = hungerStatus === 2 ? 'Starving' : hungerStatus === 1 ? 'Hungry' : 'Fine';
