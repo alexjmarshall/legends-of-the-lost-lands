221822bd4cbb6d83d29bab3a15a65b1e2b80e7c2
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2022-07-07 16:57:26 -0600

    prepar sheet data for other feature types

--- module/feature-item-sheet.js
+++ module/feature-item-sheet.js
@@ -25,12 +25,19 @@ export class FeatureItemSheet extends SimpleItemSheet {
     context.isGM = game.user.isGM;
     context.isPlayer = !game.user.isGM;
 
-    context.propA = {
-      label: 'Type',
-      value: Util.upperCaseFirst(context.data.type).replace('_',' '),
-    };
+    // const formattedFeatureTypes = {
+    //   "natural_weapon": "Weapon",
+    //   "grapple_maneuver": "Grappling",
+    // };
+    // context.propA = {
+    //   label: 'Type',
+    //   value: formattedFeatureTypes[context.data.type] || Util.upperCaseFirst(context.data.type),
+    // };
 
-    this._prepareSkillData(context)
+    this._prepareSkillData(context);
+    this._prepareFeatureData(context);
+    this._prepareGrapplingData(context);
+    this._prepareWeaponData(context);
 
     return context;
   }
@@ -41,6 +48,38 @@ export class FeatureItemSheet extends SimpleItemSheet {
     context.propB = {
       label: 'ST',
       value: Util.getDerivedSkillTarget(context.data, actorData),
+    };
+  }
+
+  _prepareFeatureData(context) {
+    if (context.data.type !== 'feature') return;
+    const source = context.data.data.attributes.source.value;
+    context.propB = {
+      label: 'Source',
+      value: source,
+    };
+  }
+
+  _prepareGrapplingData(context) {
+    if (context.data.type !== 'grapple_maneuver') return;
+    const atkReq = context.data.data.attributes.req_atk_status.value;
+    const defReq = context.data.data.attributes.req_def_status.value;
+    context.propA = {
+      label: 'Attacker',
+      value: atkReq,
+    };
+    context.propB = {
+      label: 'Defender',
+      value: defReq,
+    };
+  }
+
+  _prepareWeaponData(context) {
+    if (context.data.type !== 'natural_weapon') return;
+    const dmg = context.data.data.attributes.dmg.value;
+    context.propB = {
+      label: 'Damage',
+      value: dmg,
     };
   }
 }
