9610d4e3223dd25863760b1e05c8f7def2937348
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2022-07-06 22:37:44 -0600

    update initial tab

--- module/spell-item-sheet.js
+++ module/spell-item-sheet.js
@@ -10,7 +10,7 @@ export class SpellItemSheet extends SimpleItemSheet {
       template: "systems/lostlands/templates/spell-item-sheet.html",
       width: 520,
       height: 480,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
+      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "attributes"}],
       scrollY: [".description", ".attributes"],
     });
   }
