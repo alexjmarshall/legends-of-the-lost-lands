41be59d142d07ccd4a671aa59c29b4dc08e42e43
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2023-04-30 23:54:37 -0600

    add features.js

---
+++ rules/features.js
@@ -0,0 +1,64 @@
+// macro: macro code associated with feature for player to activate TODO
+// active_effects: active effect definitions associated with feature TODO
+export default {
+  'chain attack': {
+    req_lvl: 1,
+    max_attacks: 1,
+  },
+  'extra attack': {
+    req_lvl: 1,
+    attacks: 1,
+  },
+  berserk: {
+    req_lvl: 1,
+    // TODO definition of berserk and fatigued active effect
+  },
+  'turn undead': {
+    req_lvl: 1,
+    // TODO macro
+  },
+  'divine erudition': {
+    req_lvl: 1,
+  },
+  'spell casting': {
+    req_lvl: 1,
+    spell_type: '',
+  },
+  'scribe scrolls': {
+    req_lvl: 1,
+    spell_type: '',
+  },
+  'read scrolls': {
+    req_lvl: 1,
+    spell_type: '',
+  },
+  'read languages': {
+    req_lvl: 1,
+  },
+  'carve rune': {
+    req_lvl: 1,
+    // TODO macro and item type for runes -- can learn runes by finding them or learn 1 per level up
+  },
+  backstab: {
+    req_lvl: 1,
+    damage_multi: 2,
+  },
+  'steal spell': {
+    req_lvl: 1,
+  },
+  'sense magic': {
+    req_lvl: 2,
+  },
+  'see ethereal creatures': {
+    req_lvl: 4,
+  },
+  'sense memorized spells': {
+    req_lvl: 5,
+  },
+  'immune to level drain': {
+    req_lvl: 7,
+  },
+  'drain magic': {
+    req_lvl: 11,
+  },
+};
