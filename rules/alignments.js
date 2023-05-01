8c4696e4a29f69f01fb03ab8be03a64af46abe10
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2023-04-30 23:55:07 -0600

    add abilities and alignments

---
+++ rules/alignments.js
@@ -0,0 +1,9 @@
+export const ALIGNMENTS_ENUM = {
+  LG: 'lawful good',
+  CG: 'chaotic good',
+  N: 'neutral',
+  LE: 'lawful evil',
+  CE: 'chaotic evil',
+};
+
+export const ALL_ALIGNMENTS = Object.values(ALIGNMENTS_ENUM);
