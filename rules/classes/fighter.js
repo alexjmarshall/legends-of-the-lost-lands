aa2fde3d91b3b1a52aca46c50296122ab44d817e
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2023-06-06 23:56:17 -0600

    update fighter.js

--- rules/classes/fighter.js
+++ rules/classes/fighter.js
@@ -1,17 +1,35 @@
-import { ALL_ARMORS, ALL_SHIELD_TYPES } from '../armors';
+import { ALL_ARMORS, ALL_SHIELDS } from '../armors';
 import { ALL_WEAPONS, BASIC_SKILLS_EXCL_WEAPON, SKILL_PROGRESSIONS_ENUM, buildSkills } from '../skills';
 import { SAVES_ENUM, SAVE_PROGRESSIONS_ENUM, buildSaves } from '../saves';
 import { buildFeatures } from '../features';
 import { BaseClass } from './base-class';
+import { CLASSES_ENUM } from './classes-enum';
+import { deepFreeze } from '../helper';
 
 const { SPECIALIZED, BASIC } = SKILL_PROGRESSIONS_ENUM;
 const { GOOD, POOR } = SAVE_PROGRESSIONS_ENUM;
-const { PHYSICAL, EVASION, MENTAL, LUCK } = SAVES_ENUM;
+const { RESILIENCE, RESOLVE, EVASION, LUCK } = SAVES_ENUM;
+const { FIGHTER, BERSERKER } = CLASSES_ENUM;
 
 export class Fighter extends BaseClass {
-  #XP_REQS = [1000, 3000, 7000, 15000, 30000, 60000, 120000, 240000, 360000, 480000, 600000, 720000, 840000, Infinity];
+  #XP_REQS = Object.freeze([
+    1000,
+    3000,
+    7000,
+    15000,
+    30000,
+    60000,
+    120000,
+    240000,
+    420000,
+    600000,
+    780000,
+    960000,
+    1140000,
+    Infinity,
+  ]);
 
-  #TITLES = [
+  #TITLES = Object.freeze([
     'Veteran',
     'Warrior',
     'Swordsman',
@@ -26,21 +44,21 @@ export class Fighter extends BaseClass {
     'Lord (12th)',
     'Lord (13th)',
     'Lord (14th)',
-  ];
+  ]);
 
-  #SKILL_PROGRESSIONS = {
+  #SKILLS = deepFreeze({
     progressions: {
       [SPECIALIZED]: ALL_WEAPONS,
       [BASIC]: BASIC_SKILLS_EXCL_WEAPON,
     },
-  };
+  });
 
-  #SAVE_PROGRESSIONS = {
+  #SAVES = deepFreeze({
     progressions: {
-      [GOOD]: [PHYSICAL, LUCK],
-      [POOR]: [EVASION, MENTAL],
+      [GOOD]: [RESILIENCE, LUCK],
+      [POOR]: [RESOLVE, EVASION],
     },
-  };
+  });
 
   constructor(lvl) {
     super(lvl);
@@ -48,16 +66,16 @@ export class Fighter extends BaseClass {
     this.xpRequired = this.#XP_REQS[lvl - 1];
     this.title = this.#TITLES[lvl - 1];
     this.armors = ALL_ARMORS;
-    this.shields = ALL_SHIELD_TYPES;
-    this.skills = buildSkills(this.#SKILL_PROGRESSIONS, lvl);
-    this.saves = buildSaves(this.#SAVE_PROGRESSIONS, lvl);
-    this.features = buildFeatures('fighter', lvl);
+    this.shields = ALL_SHIELDS;
+    this.skills = buildSkills(this.#SKILLS, lvl);
+    this.saves = buildSaves(this.#SAVES, lvl);
+    this.features = buildFeatures(FIGHTER, lvl);
   }
 }
 
 export class Berserker extends Fighter {
   constructor(lvl) {
     super(lvl);
-    this.features = buildFeatures('berserker', lvl);
+    this.features = buildFeatures(BERSERKER, lvl);
   }
 }
