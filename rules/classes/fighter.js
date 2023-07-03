72a5b9fa3c6fa4f5582aa3d057d81ebb04fee787
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2023-07-02 23:27:08 -0600

    update fighter class

--- rules/classes/fighter.js
+++ rules/classes/fighter.js
@@ -1,18 +1,13 @@
-import { ALL_ARMORS, ALL_SHIELDS } from '../armors';
-import { ALL_WEAPONS, BASIC_SKILLS_EXCL_WEAPON, SKILL_PROGRESSIONS_ENUM, buildSkills } from '../skills';
-import { SAVES_ENUM, SAVE_PROGRESSIONS_ENUM, buildSaves } from '../saves';
-import { buildFeatures } from '../features';
+import { allArmorsArray, allShieldsArray } from '../armors';
+import { allCombatSkillsArray, skillsEnum } from '../skills';
+import { saveModGroups } from '../saves';
+import { ClassFeature, featuresEnum } from '../features';
 import { BaseClass } from './base-class';
-import { CLASSES_ENUM } from './classes-enum';
+import { abilitiesEnum } from '../abilities';
 import { deepFreeze } from '../helper';
 
-const { SPECIALIZED, BASIC } = SKILL_PROGRESSIONS_ENUM;
-const { GOOD, POOR } = SAVE_PROGRESSIONS_ENUM;
-const { RESILIENCE, RESOLVE, EVASION, LUCK } = SAVES_ENUM;
-const { FIGHTER, BERSERKER } = CLASSES_ENUM;
-
 export class Fighter extends BaseClass {
-  #XP_REQS = Object.freeze([
+  static XP_REQS = Object.freeze([
     1000,
     3000,
     7000,
@@ -21,15 +16,15 @@ export class Fighter extends BaseClass {
     60000,
     120000,
     240000,
-    420000,
-    600000,
-    780000,
-    960000,
-    1140000,
+    400000,
+    560000,
+    720000,
+    880000,
+    1040000,
     Infinity,
   ]);
 
-  #TITLES = Object.freeze([
+  static TITLES = Object.freeze([
     'Veteran',
     'Warrior',
     'Swordsman',
@@ -46,36 +41,58 @@ export class Fighter extends BaseClass {
     'Lord (14th)',
   ]);
 
-  #SKILLS = deepFreeze({
-    progressions: {
-      [SPECIALIZED]: ALL_WEAPONS,
-      [BASIC]: BASIC_SKILLS_EXCL_WEAPON,
-    },
-  });
+  static features = deepFreeze([
+    new ClassFeature(featuresEnum.CHAIN_ATTACK, 1, (lvl) => ({
+      maxAttacks: lvl,
+    })),
+    new ClassFeature(featuresEnum.MULTIATTACK, 7, (lvl) => ({
+      extraAttacks: BaseClass.onePerNLevelsAfterFirst(lvl, 6),
+    })),
+  ]);
 
-  #SAVES = deepFreeze({
-    progressions: {
-      [GOOD]: [RESILIENCE, LUCK],
-      [POOR]: [RESOLVE, EVASION],
-    },
-  });
+  static specializedSkills = Object.freeze([...allCombatSkillsArray]);
+  static proficientSkills = [];
+
+  static saveMods = saveModGroups.fighter;
 
   constructor(lvl) {
-    super(lvl);
+    lvl = Number(lvl);
+    super(lvl, Fighter);
+    this.primeReqs = [abilitiesEnum.STR];
     this.hitDie = 'd8';
-    this.xpRequired = this.#XP_REQS[lvl - 1];
-    this.title = this.#TITLES[lvl - 1];
-    this.armors = ALL_ARMORS;
-    this.shields = ALL_SHIELDS;
-    this.skills = buildSkills(this.#SKILLS, lvl);
-    this.saves = buildSaves(this.#SAVES, lvl);
-    this.features = buildFeatures(FIGHTER, lvl);
+    this.reqXp = Fighter.XP_REQS[lvl - 1];
+    this.title = Fighter.TITLES[lvl - 1];
+    this.armors = [...allArmorsArray];
+    this.shields = [...allShieldsArray];
+    this.abilityReqs = {
+      [abilitiesEnum.STR]: {
+        min: 9,
+      },
+    };
   }
 }
 
 export class Berserker extends Fighter {
+  static features = deepFreeze([
+    new ClassFeature(featuresEnum.BERSERK, 1, (lvl) => ({
+      usesPerDay: BaseClass.onePlusOnePerFourLevels(lvl),
+    })),
+    new ClassFeature(featuresEnum.MULTIATTACK, 7, (lvl) => ({
+      extraAttacks: BaseClass.onePerNLevelsAfterFirst(lvl, 6),
+    })),
+  ]);
+
   constructor(lvl) {
+    lvl = Number(lvl);
     super(lvl);
-    this.features = buildFeatures(BERSERKER, lvl);
+    this.abilityReqs = {
+      [abilitiesEnum.STR]: {
+        min: 9,
+      },
+      [abilitiesEnum.WIS]: {
+        max: 12,
+      },
+    };
+    this.buildFeatures(Berserker, lvl);
   }
 }
