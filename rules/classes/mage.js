64a6e21970cf99a50f8f9ac082bc2b2b42043156
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2023-07-02 23:28:00 -0600

    update mage class

--- rules/classes/mage.js
+++ rules/classes/mage.js
@@ -1,24 +1,12 @@
-import {
-  WEAP_SKILLS_ENUM,
-  ALL_SPELL_SCHOOLS,
-  SKILL_PROGRESSIONS_ENUM,
-  BASIC_SKILLS_EXCL_WEAPON,
-  buildSkills,
-} from '../skills.js';
-import { SAVES_ENUM, SAVE_PROGRESSIONS_ENUM, buildSaves } from '../saves';
-import { buildFeatures } from '../features';
+import { allSpellSkillsArray, skillsEnum } from '../skills';
+import { saveModGroups } from '../saves';
+import { ClassFeature, featuresEnum } from '../features';
 import { BaseClass } from './base-class';
-import { CLASSES_ENUM } from './classes-enum';
+import { abilitiesEnum } from '../abilities';
 import { deepFreeze } from '../helper';
 
-const { SPECIALIZED, BASIC } = SKILL_PROGRESSIONS_ENUM;
-const { GOOD, POOR } = SAVE_PROGRESSIONS_ENUM;
-const { RESILIENCE, RESOLVE, EVASION, LUCK } = SAVES_ENUM;
-const { DAGGER, SLING, STAFF, THROW } = WEAP_SKILLS_ENUM;
-const { MAGE, INCANTATRIX, AIR_ELEMENTALIST, FIRE_ELEMENTALIST, EARTH_ELEMENTALIST, WATER_ELEMENTALIST } = CLASSES_ENUM;
-
 export class Mage extends BaseClass {
-  #XP_REQS = Object.freeze([
+  static XP_REQS = Object.freeze([
     1200,
     3600,
     8400,
@@ -29,13 +17,13 @@ export class Mage extends BaseClass {
     100000,
     200000,
     300000,
-    450000,
-    600000,
-    750000,
+    500000,
+    700000,
+    900000,
     Infinity,
   ]);
 
-  #TITLES = Object.freeze([
+  static TITLES = Object.freeze([
     'Medium',
     'Seer',
     'Conjurer',
@@ -52,7 +40,7 @@ export class Mage extends BaseClass {
     'Wizard (14th)',
   ]);
 
-  _SPELL_SLOTS_BY_LEVEL = deepFreeze([
+  static SPELL_SLOTS_BY_LEVEL = deepFreeze([
     [1],
     [2],
     [3, 1],
@@ -69,72 +57,94 @@ export class Mage extends BaseClass {
     [5, 5, 5, 4, 4, 3, 1],
   ]);
 
-  #SKILLS = deepFreeze({
-    progressions: {
-      [SPECIALIZED]: ALL_SPELL_SCHOOLS,
-      [BASIC]: [...BASIC_SKILLS_EXCL_WEAPON, DAGGER, SLING, STAFF, THROW],
-    },
-  });
+  static features = deepFreeze([
+    new ClassFeature(featuresEnum.CAST_MAGICK_SPELLS, 1),
+    new ClassFeature(featuresEnum.READ_MAGICK_SCROLLS, 1),
+    new ClassFeature(featuresEnum.SCRIBE_MAGICK_SCROLLS, 1),
+  ]);
 
-  #SAVES = deepFreeze({
-    mod: -1,
-    progressions: {
-      [GOOD]: [RESOLVE, EVASION],
-      [POOR]: [RESILIENCE, LUCK],
-    },
-  });
+  static specializedSkills = Object.freeze([...allSpellSkillsArray]);
+  static proficientSkills = Object.freeze([skillsEnum.DAGGER, skillsEnum.SLING, skillsEnum.STAFF, skillsEnum.THROW]);
+
+  static saveMods = saveModGroups.mage;
 
   constructor(lvl) {
-    super(lvl);
+    lvl = Number(lvl);
+    super(lvl, Mage);
+    this.primeReqs = [abilitiesEnum.INT];
     this.hitDie = 'd4';
-    this.xpRequired = this.#XP_REQS[lvl - 1];
-    this.title = this.#TITLES[lvl - 1];
-    this.spellSlots = this.SPELL_SLOTS_BY_LEVEL[lvl - 1];
-    this.skills = buildSkills(this.#SKILLS, lvl);
-    this.saves = buildSaves(this.#SAVES, lvl);
-    this.features = buildFeatures(MAGE, lvl);
+    this.reqXp = Mage.XP_REQS[lvl - 1];
+    this.title = Mage.TITLES[lvl - 1];
+    this.spellSlots = Mage.SPELL_SLOTS_BY_LEVEL[lvl - 1];
+    this.abilityReqs = {
+      [abilitiesEnum.INT]: {
+        min: 9,
+      },
+    };
+    this.buildSkills(Mage);
+    this.buildSaves(Mage);
+    this.buildFeatures(Mage, lvl);
   }
 }
 
 export class Incantatrix extends Mage {
+  static features = deepFreeze([
+    ...super.features,
+    new ClassFeature(featuresEnum.STEAL_SPELL, 1),
+    new ClassFeature(featuresEnum.SENSE_SPELL, 3),
+    new ClassFeature(featuresEnum.SENSE_MEMORIZED_SPELLS, 5),
+    new ClassFeature(featuresEnum.SENSE_MAGICK, 7),
+    new ClassFeature(featuresEnum.DRAIN_MAGICK, 11),
+  ]);
+
+  static SPELL_SLOTS_BY_LEVEL = Object.freeze([...BaseClass.removeOneSpellSlotPerLevel(Mage.SPELL_SLOTS_BY_LEVEL)]);
+
   constructor(lvl) {
+    lvl = Number(lvl);
     super(lvl);
-    this.spellSlots = super.removeOneSpellSlotPerLevel(super._SPELL_SLOTS_BY_LEVEL);
-    this.features = buildFeatures(INCANTATRIX, lvl);
+    this.spellSlots = Incantatrix.SPELL_SLOTS_BY_LEVEL[lvl - 1];
+    this.buildFeatures(Incantatrix, lvl);
   }
 }
 
 class BaseElementalist extends Mage {
+  static features = deepFreeze([
+    ...super.features,
+    new ClassFeature(featuresEnum.ELEMENTAL_SURGE, 1),
+    new ClassFeature(featuresEnum.ELEMENTAL_FOCUS, 1),
+  ]);
+  // TODO the element type will be derived data of feature item based on class name
+
+  static SPELL_SLOTS_BY_LEVEL = Object.freeze([...BaseClass.addOneSpellSlotPerLevel(Mage.SPELL_SLOTS_BY_LEVEL)]);
+
   constructor(lvl) {
+    lvl = Number(lvl);
     super(lvl);
-    this.spellSlots = super.addOneSpellSlotPerLevel(super._SPELL_SLOTS_BY_LEVEL);
+    this.spellSlots = BaseElementalist.SPELL_SLOTS_BY_LEVEL[lvl - 1];
+    this.buildFeatures(BaseElementalist, lvl);
   }
 }
 
 export class AirElementalist extends BaseElementalist {
   constructor(lvl) {
     super(lvl);
-    this.features = buildFeatures(AIR_ELEMENTALIST, lvl);
   }
 }
 
 export class FireElementalist extends BaseElementalist {
   constructor(lvl) {
     super(lvl);
-    this.features = buildFeatures(FIRE_ELEMENTALIST, lvl);
   }
 }
 
 export class EarthElementalist extends BaseElementalist {
   constructor(lvl) {
     super(lvl);
-    this.features = buildFeatures(EARTH_ELEMENTALIST, lvl);
   }
 }
 
 export class WaterElementalist extends BaseElementalist {
   constructor(lvl) {
     super(lvl);
-    this.features = buildFeatures(WATER_ELEMENTALIST, lvl);
   }
 }
